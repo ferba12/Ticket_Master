9ece2b6406496bf338ab1e8b39f34e18623b1ffe
FernandoBarroso <ferbad12@hotmail.com>
2014-10-31 23:00:56 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicketMaster::Application.config.secret_token = '7214bbe664ae3ea8e8193a7538ab64b9e8d2d79ee26f6c3944cda82e81f8735c03e01a53ccd4af236924a797f4b1d0bcbadc75d85e11cd4a6db88e000f83fdd8'
