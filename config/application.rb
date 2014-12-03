d15a92f54171881ca44e842dd1c1cb6caa7f70f8
FernandoBarroso <ferbad12@hotmail.com>
2014-12-03 11:54:39 -0300

    Update Gemfile.lock for Heroku 3

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module TicketMaster
   class Application < Rails::Application
 
     config.generators.test_framework false
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
